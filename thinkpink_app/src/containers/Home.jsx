d0dd63cd0dfbb444f536a7cf2fb48fefc6952870
anastasiakrouglova <anastasia.krouglova@student.howest.be>
2020-01-29 21:55:41 +0100

    basics video

--- thinkpink_app/src/containers/Home.jsx
+++ thinkpink_app/src/containers/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "./Home.module.css";
 import NavbarDark from "../components/NavbarDark.jsx";
+import Video from "../components/Video.jsx"
 
 import { render } from 'react-dom'
 import { VideoScroll } from 'react-video-scroll'
+
+import { Controller, Scene } from "react-scrollmagic";
 
 // const setStyles = (wrapperEl, videoEl, playbackRate) => {
 //   wrapperEl.style.marginTop = `calc(180% - ${Math.floor(videoEl.duration) *
@@ -14,28 +17,37 @@ import { VideoScroll } from 'react-video-scroll'
 //     'px'})`
 // }
 
-function Home () {
+function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.navbar}><NavbarDark className={styles.navbar} /></div>
       <p className={styles.title}>Race For The Cure</p>
-      <VideoScroll
+      <Controller>
+        <Scene duration="200%" triggerHook="onLeave" pin>
+            {progress => (
+            <div style={{ height: "100vh", position: "relative" }}>
+              <Video progress={progress}/>
+              </div>
+            )}
+          </Scene>
+      </Controller>  
+      {/* <VideoScroll
         // onLoad={props =>
         //   setStyles(props.wrapperEl, props.videoEl, props.playbackRate)
         // }
         playbackRate={30}
         style={{ position: 'sticky' }}
       >
-        <video
+        <video className={styles.section}
           tabIndex="0"
           autobuffer="autobuffer"
           preload="preload"
-          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          // style={{ width: '100%', height: '100%', objectFit: 'cover' }}
           playsInline
         >
           <source type="video/mp4" src="../assets/video/test.mp4" />
         </video>
-      </VideoScroll>
+      </VideoScroll> */}
     
       {/*
       <button className="App-button_primair">Primaire button</button>
