381dff11c80188979240371e17ad2d31fbcdebd8
Robbe Buyle <robbe.buyle@telenet.be>
2020-01-30 10:15:53 +0100

    change project structure to support server logic

--- thinkpink_app/client/src/containers/hooks.jsx
+++ thinkpink_app/client/src/containers/hooks.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, setState } from "react";
 
 const defaultState = { image: undefined, status: "loading" };
 export const useImage = (url, crossOrigin) => {
