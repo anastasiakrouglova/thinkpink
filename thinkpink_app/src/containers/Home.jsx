ef32e3fc3761900a6a6c58824ea19660e9a64743
anastasiakrouglova <anastasia.krouglova@student.howest.be>
2020-01-30 09:55:40 +0100

    css button home

--- thinkpink_app/src/containers/Home.jsx
+++ thinkpink_app/src/containers/Home.jsx
@@ -1,60 +1,36 @@
 import React, { useRef } from "react";
 import styles from "./Home.module.css";
 import NavbarDark from "../components/NavbarDark.jsx";
-import Video from "../components/Video.jsx"
-
-import { render } from 'react-dom'
-import { VideoScroll } from 'react-video-scroll'
 
 import { Controller, Scene } from "react-scrollmagic";
 import Sequence from "../components/Sequence.jsx";
-
-// const setStyles = (wrapperEl, videoEl, playbackRate) => {
-//   wrapperEl.style.marginTop = `calc(180% - ${Math.floor(videoEl.duration) *
-//     playbackRate +
-//     'px'})`
-//   wrapperEl.style.marginBottom = `calc(180% - ${Math.floor(videoEl.duration) *
-//     playbackRate +
-//     'px'})`
-// }
 
 function Home() {
   const ref = useRef();
   return (
     <div className={styles.container}>
       <div className={styles.navbar}><NavbarDark className={styles.navbar} /></div>
-      <p className={styles.title}>Race For The Cure</p>
+      <section>
+        <p className={styles.title}>Race For The Cure</p>
+        <button className={styles.primair_button}>Schrijf je in voor deze race</button>
+      </section>
+
+      <section>
+        <p>01</p>
+        <p>Wat is race for the cure?</p>
+        <p>Race For The Cure is een sportief evenement waarin solidariteit een belangrijke rol speelt in het gevecht tegen borstkanker. De race is origineel onstaat vanuit een initiatief van ‘Think Pink’’. Deze race heeft zich ondertussen uitgebreid over meer dan 20 landen in het Europees continent. De hele dag is gevuld met animatie en intense emoties.</p>
+        {/* <video src="../assets/video/test.mp4"></video> */}
+      </section>
+      
       <Controller>
         <Scene duration="200%" triggerHook="onLeave" pin>
             {progress => (
             <div style={{ height: "100vh", position: "relative" }}>
-              {/* <Video ref={ref} progress={progress}/> */}
               <Sequence ref={ref} progress={progress} />
               </div>
             )}
           </Scene>
       </Controller>  
-      {/* <VideoScroll
-        // onLoad={props =>
-        //   setStyles(props.wrapperEl, props.videoEl, props.playbackRate)
-        // }
-        playbackRate={30}
-        style={{ position: 'sticky' }}
-      >
-        <video className={styles.section}
-          tabIndex="0"
-          autobuffer="autobuffer"
-          preload="preload"
-          // style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-          playsInline
-        >
-          <source type="video/mp4" src="../assets/video/test.mp4" />
-        </video>
-      </VideoScroll> */}
-    
-      {/*
-      <button className="App-button_primair">Primaire button</button>
-      <a class={styles.play_btn} href="#"></a> */}
     </div>
   );
 };
