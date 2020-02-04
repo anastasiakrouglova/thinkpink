dcb384558f97b77a5caebccd69111cf6054fd02e
anastasiakrouglova <anastasia.krouglova@student.howest.be>
2020-02-04 18:43:47 +0100

    css formlotgenoot 40% ready

--- thinkpink_app/client/src/containers/SurvivorOrTeamcaptain.jsx
+++ thinkpink_app/client/src/containers/SurvivorOrTeamcaptain.jsx
@@ -1,35 +1,50 @@
 import React from "react";
-// import styles from "./Info.module.css";
+import styles from "./SurvivorOrTeamcaptain.module.css";
 import NavbarWhite from "../components/NavbarWhite.jsx";
-import Race from "../components/Race.jsx"
+import Race from "../components/Race.jsx";
 import { NavLink } from "react-router-dom";
 import { ROUTES } from "../constants";
 
 const Join = () => {
   return (
     <div>
-        <div>
-          <NavbarWhite />
-          <h1>Bedankt voor je enthousiasme!</h1>
-          <p>kies hoe je een lotgenoot wilt toevoegen:</p>
+      <div>
+        <NavbarWhite />
+        <h1 className={styles.title}>Bedankt voor je enthousiasme!</h1>
+        <p className={styles.subtitle}>kies hoe je een lotgenoot wilt toevoegen:</p>
+        <div className={styles.container_cards}>
 
-          <div>
-          <NavLink to={ROUTES.lotgenoot2}>
-              <p>Ik ben een <span>lotgenoot</span> en wil mijn eigen pagina onderhouden</p>
-              <p>€15</p>
-              <button>registreer lotgenoot</button>
-            </NavLink>
-            <NavLink to={ROUTES.lotgenoot2}>
-            <p>Ik ben een <span>Team Captain</span> en wil een pagina aanmaken voor een lotgenoot die ik ken</p>
-              <p>excl. ticket lotgenoot</p>
-              <p>€15</p>
-              <button>registreer een teamcaptain</button>
+          <div className={styles.cardlotgenoot}>
+            <NavLink className={styles.cardlotgenoot_navlink} to={ROUTES.lotgenoot2}>
+              <p className={styles.infotext}>
+                Ik ben een <span>lotgenoot</span> en wil mijn eigen pagina
+                onderhouden
+              </p>
+              <p className={styles.price}>€15</p>
+              <div className={styles.container_button}>
+                <button className={styles.card_button}>registreer lotgenoot</button>
+              </div>
             </NavLink>
           </div>
-          <div>
-          <NavLink to={ROUTES.detail}>terug</NavLink>
+          <div className={styles.cardteamcaptain}>
+            <NavLink className={styles.cardlotgenoot_navlink} to={ROUTES.lotgenoot2}>
+              <p className={styles.infotext}>
+                Ik ben een <span>Team Captain</span> en wil een pagina aanmaken
+                voor een lotgenoot die ik ken
+              </p>
+              <p className={styles.exclusief}>excl. ticket lotgenoot</p>
+              <p className={styles.price}>€15</p>
+              <div className={styles.container_button}>
+                <button className={styles.card_button}>registreer een teamcaptain</button>
+              </div>
+            </NavLink>
           </div>
+
         </div>
+        <div>
+          <NavLink to={ROUTES.detail}>terug</NavLink>
+        </div>
+      </div>
     </div>
   );
 };
