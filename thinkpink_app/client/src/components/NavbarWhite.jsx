8399e3984a6ae3d43d2bbf4db89d0ba6bab48a6c
anastasiakrouglova <anastasia.krouglova@student.howest.be>
2020-02-10 12:18:48 +0100

    login header changes

--- thinkpink_app/client/src/components/NavbarWhite.jsx
+++ thinkpink_app/client/src/components/NavbarWhite.jsx
@@ -2,16 +2,27 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { ROUTES } from "../constants";
 // import logo from '../logo.svg';
+import { inject } from "mobx-react";
 import styles from './NavbarWhite.module.css';
 
 
-const NavbarWhite = () => {
+const NavbarWhite = ({ uiStore }) => {
+    console.log(uiStore.authUser.name);
     return (
         <div className={styles.nav_container}>
                 <NavLink to={ROUTES.home}><img src="../assets/images/logo.svg"className="App-logo" alt="logo" /></NavLink> 
                 <div className={styles.nav_link}>
-                    <NavLink className={styles.nav_link__text} activeClassName={styles.nav_link__active} to={ROUTES.races}>Kies een race</NavLink>
+                <NavLink className={styles.nav_link__text} activeClassName={styles.nav_link__active} to={ROUTES.races}>Kies een race</NavLink>
+                {!uiStore.authUser ? (
+                    <>
                     <NavLink className={styles.nav_link__text} activeClassName={styles.nav_link__active} to={ROUTES.login}><img className={styles.personIcon} src="../assets/images/icons/peopleBlack.svg" alt="icon man"/>Log in</NavLink>
+                        </>
+                ): (
+                        <>
+                            <NavLink className={styles.nav_link__text} activeClassName={styles.nav_link__active} to='#'><img className={styles.personIcon} src="../assets/images/icons/peopleBlack.svg" alt="icon man"/>{uiStore.authUser.name}</NavLink>
+                            </>
+                )
+}
                     <select className={styles.nav_link__dropdown}>
                     <option value="numberCountry">🇧🇪&nbsp; Nederlands &nbsp;</option>
                         <option value="numberCountry">🇨🇮&nbsp; Italiano &nbsp;</option>
@@ -24,4 +35,4 @@ const NavbarWhite = () => {
     )
 }
 
-export default NavbarWhite
+export default inject(`uiStore`)(NavbarWhite);
